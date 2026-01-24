4dcd711e4960204a3474d418358c8d97db8351dd
Banksy <445668+84nk5y@users.noreply.github.com>
2026-01-24 11:22:41 +0100

    Added fade effect when out of combat

--- core.lua
+++ core.lua
@@ -22,3 +22,42 @@ hooksecurefunc(PersonalResourceDisplayFrame, "SetupAlternatePowerBar", function(
         local classColor = RAID_CLASS_COLORS[englishClass]
     prdHealthBar:SetStatusBarColor(classColor.r, classColor.g, classColor.b)
 end)
+
+local f = CreateFrame("Frame")
+f:RegisterEvent("PLAYER_ENTERING_WORLD")
+f:RegisterEvent("PLAYER_REGEN_DISABLED")
+f:RegisterEvent("PLAYER_REGEN_ENABLED")
+
+local function ApplyFading()
+    local prd = PersonalResourceDisplayFrame
+    local debuffs = DebuffFrame
+    local cdbuffs = BuffIconCooldownViewer
+
+    if not prd then
+        print("no PersonalResourceDisplayFrame")
+        return
+    elseif not debuffs then
+        print("no DebuffFrame")
+        return
+    elseif not cdbuffs then
+        print("no BuffIconCooldownViewer")
+        return
+    end
+
+    if UnitAffectingCombat("player") then
+        -- Fade to 100% opacity over 0.2 seconds
+        UIFrameFadeIn(prd, 0.2, prd:GetAlpha(), 1)
+        UIFrameFadeIn(debuffs, 0.2, debuffs:GetAlpha(), 1)
+        UIFrameFadeIn(cdbuffs, 0.2, cdbuffs:GetAlpha(), 1)
+    else
+        -- Fade to 10% opacity over 0.2 seconds
+        UIFrameFadeOut(prd, 0.2, prd:GetAlpha(), 0.05)
+        UIFrameFadeOut(debuffs, 0.2, debuffs:GetAlpha(), 0.1)
+        UIFrameFadeOut(cdbuffs, 0.2, cdbuffs:GetAlpha(), 0.1)
+    end
+end
+
+f:SetScript("OnEvent", function(self, event)
+    -- Small delay to ensure frames exists
+    C_Timer.After(0.1, ApplyFading)
+end)
